a2baecfd5deaf4aca1737ce24738cb331ff00e27
Patrick VanDusen <pdusen@gmail.com>
2018-07-30 18:03:41 -0400

    Added arguments test

--- pool/pool.py
+++ pool/pool.py
@@ -8,11 +8,11 @@ class Pool:
         self.bot = bot
 
     @commands.command()
-    async def mycom(self):
+    async def mycom(self, arg1, arg2):
         """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff! arg1: " + arg1 + "; arg2: " + arg2)
 
 def setup(bot):
     bot.add_cog(Pool(bot))
