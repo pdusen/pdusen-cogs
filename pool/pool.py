d9c2d18c1a8da69933ba51990e2d09c207d72841
Patrick VanDusen <pdusen@gmail.com>
2018-07-30 18:41:48 -0400

    Starting to do real dicerolls

--- pool/pool.py
+++ pool/pool.py
@@ -1,18 +1,24 @@
-import discord
+import discord, re, dice
 from discord.ext import commands
 
 class Pool:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
+        self.pattern = re.compile('^(\d*)\!?(\d)*$')
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, *, arg):
+    async def pool(self, *, arg):
         """This does stuff!"""
 
-        #Your code will go here
-        await self.bot.say("I can do stuff! arg: " + arg)
+        match = self.pattern.match(arg)
+        if not match:
+            await self.bot.say("You did it wrong!")
+            return
+
+        pool_size = int(match.group(1))
+        await self.bot.say('Results: ' + dice.roll(pool_size + 'd10'))
 
 def setup(bot):
     bot.add_cog(Pool(bot))
